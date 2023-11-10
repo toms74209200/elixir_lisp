a472790a18c3ed3080c91d0179107d423637d635
toms74209200 <motomotomato@gmail.com>
2023-11-11 01:45:07 +0900

    :heavy_plus_sign: add credo

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule ElixirLisp.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 end
