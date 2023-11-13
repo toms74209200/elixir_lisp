23018b25d89b4c7a763f81f14eff57f53e676b1d
toms74209200 <motomotomato@gmail.com>
2023-11-13 23:27:54 +0900

    :sparkles: add tokenizing parsed list

--- test/paser_test.exs
+++ test/paser_test.exs
@@ -1,4 +1,20 @@
 defmodule PaserTest do
   use ExUnit.Case
   doctest Parser
+
+  test "normal" do
+    assert Parser.parse("(+ 1 2)") |> Parser.tokenize() == {:ok, [:+, 1, 2]}
+  end
+
+  test "normal with nested" do
+    assert Parser.parse("(* (+ 1 2) 3)") |> Parser.tokenize() == {:ok, [:*, [:+, 1, 2], 3]}
+  end
+
+  test "empty with input argument is empty string" do
+    assert Parser.parse("") |> Parser.tokenize() == {:ok, []}
+  end
+
+  test "error with input argumant is not string" do
+    assert Parser.parse(1) |> Parser.tokenize() == {:error, "Input argument must be a string."}
+  end
 end
